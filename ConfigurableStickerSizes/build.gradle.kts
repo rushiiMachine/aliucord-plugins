f7566fd270ab1a72212230191039542c3a8d95bd
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-10-25 15:28:23 -0700

    chore(StickerSizes): release

--- ConfigurableStickerSizes/build.gradle.kts
+++ ConfigurableStickerSizes/build.gradle.kts
@@ -1,8 +1,12 @@
-version = "1.0.1"
+version = "1.1.0"
 description = "Makes sticker sizes configurable."
 
 aliucord {
     changelog.set("""
+        # 1.1.0
+        * Reset button to change to 240px
+        * Move stickers upwards to fill gap
+        
         # 1.0.1
         * Fix text size
         
