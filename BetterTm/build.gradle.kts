1a31b5e8a67d368a3cbdb0761392dd13474c8b7e
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-11-30 19:44:06 -0800

    feat(BetterTm): add © and ®

--- BetterTm/build.gradle.kts
+++ BetterTm/build.gradle.kts
@@ -1,9 +1,12 @@
-version = "1.0.0"
-description = "Replaces the TM emoji with one that's visible."
+version = "1.1.0"
+description = "Replaces the TM and R emoji with one that's more visible."
 
 aliucord {
     changelog.set(
         """
+        # 1.1.0
+        * Add Registered Trademark & Copyright emoji
+
         # 1.0.0
 		* Released
         """.trimIndent()
--- BetterTm/src/main/kotlin/com.github.diamondminer88.plugins/BetterTm.kt
+++ BetterTm/src/main/kotlin/com.github.diamondminer88.plugins/BetterTm.kt
@@ -6,7 +6,16 @@ import com.aliucord.entities.Plugin
 import com.aliucord.patcher.PreHook
 import com.discord.utilities.images.MGImages
 
+const val BASE_URL = "https://raw.githubusercontent.com/DiamondMiner88/aliucord-plugins/master/BetterTm/"
+
 const val TM_EMOJI_URI = "res:///2131823862"
+const val TM_URL = BASE_URL + "tm.png"
+
+const val R_EMOJI_URI = "res:///2131824085"
+const val R_URL = BASE_URL + "r.png"
+
+const val C_EMOJI_URI = "res:///2131824084"
+const val C_URL = BASE_URL + "c.png"
 
 @Suppress("unused")
 @AliucordPlugin(requiresRestart = true)
@@ -19,9 +28,15 @@ class BetterTm : Plugin() {
             Integer.TYPE,
             java.lang.Boolean.TYPE
         ), PreHook {
-            if (it.args[0] != TM_EMOJI_URI) return@PreHook
+//            logger.info(it.args[0] as String)
 
-            it.args[0] = "https://raw.githubusercontent.com/DiamondMiner88/aliucord-plugins/master/BetterTm/tm.png"
+            it.args[0] = when (it.args[0]) {
+                TM_EMOJI_URI -> TM_URL
+                R_EMOJI_URI -> R_URL
+                C_EMOJI_URI -> C_URL
+                else -> return@PreHook
+            }
+
             it.args[1] = 100
             it.args[2] = 100
         })
