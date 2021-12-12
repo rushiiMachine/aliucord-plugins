8d30fe61773431cc884d7d31ba2826f784c6416b
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-12-12 12:31:14 -0800

    feat(NormalizeNames): use android normalizer

--- NormalizeNames/src/main/kotlin/com.github.diamondminer88.plugins/NormalizeNames.kt
+++ NormalizeNames/src/main/kotlin/com.github.diamondminer88.plugins/NormalizeNames.kt
@@ -1,26 +1,29 @@
 package com.github.diamondminer88.plugins
 
 import android.content.Context
+import android.icu.text.Normalizer2
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.after
 import com.discord.api.guildmember.GuildMember
 import com.discord.api.user.User
-import java.text.Normalizer
 
 @Suppress("unused")
 @AliucordPlugin
 class NormalizeNames : Plugin() {
     override fun start(ctx: Context) {
+        val normalizer = Normalizer2.getNFKCInstance()
+            ?: error("Failed to get normalizer!")
+
         patcher.after<User>("r") {
             if (it.result != null) {
-                it.result = Normalizer.normalize(it.result as String, Normalizer.Form.NFKC)
+                it.result = normalizer.normalize(it.result as String)
             }
         }
 
         patcher.after<GuildMember>("g") {
             if (it.result != null) {
-                it.result = Normalizer.normalize(it.result as String, Normalizer.Form.NFKC)
+                it.result = normalizer.normalize(it.result as String)
             }
         }
     }
