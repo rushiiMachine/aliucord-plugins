d1537b66aa67e411831e1ecd966b68f4283d6152
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-06-03 18:15:20 -0700

    chore: update deps/gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 	dependencies {
 		classpath("com.android.tools.build:gradle:7.0.4")
 		classpath("com.aliucord:gradle:main-SNAPSHOT")
-		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 	}
 }
 
