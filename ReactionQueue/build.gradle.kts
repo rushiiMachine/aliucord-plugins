dd03255b9dd41b3ceac2d016443fae7fe9451868
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-06-20 13:23:29 -0700

    fix(ReactionQueue): simplify logic

--- ReactionQueue/build.gradle.kts
+++ ReactionQueue/build.gradle.kts
@@ -1,9 +1,13 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Queue up reactions to avoid hitting the ratelimit."
 
 aliucord {
 	changelog.set(
 		"""
+			# 1.0.1
+			* Improve reflection
+			* Use single threaded executor as queue
+
 			# 1.0.0
 			* Released
 		""".trimIndent()
--- ReactionQueue/src/main/kotlin/com.github.diamondminer88.plugins/ReactionQueue.kt
+++ ReactionQueue/src/main/kotlin/com.github.diamondminer88.plugins/ReactionQueue.kt
@@ -1,7 +1,6 @@
 package com.github.diamondminer88.plugins
 
 import android.content.Context
-import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.instead
@@ -10,72 +9,54 @@ import com.discord.utilities.rest.RestAPI
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import rx.Observable
-import java.lang.reflect.Member
-import java.util.*
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.Executors
 
 @Suppress("unused")
 @AliucordPlugin
 class ReactionQueue : Plugin() {
-	private val events = LinkedList<Triple<Member, Any, Array<Any>>>()
-	private var isHandlingEvents = AtomicBoolean(false)
+	private val queueWorker = Executors.newSingleThreadExecutor()
 
-	private fun recordEvent(param: XC_MethodHook.MethodHookParam) {
-		events.addLast(Triple(
-			param.method,
-			param.thisObject,
-			param.args,
-		))
-		startHandling()
+	private fun handleHook(param: XC_MethodHook.MethodHookParam) {
+		queueWorker.execute {
+			val result = XposedBridge.invokeOriginalMethod(param.method, param.thisObject, param.args)
+			(result as Observable<*>).await()
+			Thread.sleep(500)
+		}
 	}
 
 	override fun start(ctx: Context) {
+		val longPrimitiveType = Long::class.javaPrimitiveType!!
+		val stringType = String::class.java
+
 		patcher.instead<RestAPI>(
 			"addReaction",
-			Long::class.javaPrimitiveType!!,
-			Long::class.javaPrimitiveType!!,
-			String::class.java,
+			longPrimitiveType,
+			longPrimitiveType,
+			stringType,
 		) {
-			recordEvent(it)
+			handleHook(it)
 			Observable<Void> {}
 		}
 
 		patcher.instead<RestAPI>(
 			"removeReaction",
-			Long::class.javaPrimitiveType!!,
-			Long::class.javaPrimitiveType!!,
-			String::class.java,
-			Long::class.javaPrimitiveType!!,
+			longPrimitiveType,
+			longPrimitiveType,
+			stringType,
+			longPrimitiveType,
 		) {
-			recordEvent(it)
+			handleHook(it)
 			Observable<Void> {}
 		}
 
 		patcher.instead<RestAPI>(
 			"removeSelfReaction",
-			Long::class.javaPrimitiveType!!,
-			Long::class.javaPrimitiveType!!,
-			String::class.java,
+			longPrimitiveType,
+			longPrimitiveType,
+			stringType,
 		) {
-			recordEvent(it)
+			handleHook(it)
 			Observable<Void> {}
-		}
-	}
-
-	private fun startHandling() {
-		if (!isHandlingEvents.compareAndSet(false, true))
-			return
-
-		Utils.threadPool.execute {
-			while (events.isNotEmpty()) {
-				val (method, thisObject, args) = events[0]
-				(XposedBridge.invokeOriginalMethod(method, thisObject, args) as Observable<*>).await()
-
-				events.removeFirst()
-				Thread.sleep(500)
-			}
-
-			isHandlingEvents.set(false)
 		}
 	}
 
