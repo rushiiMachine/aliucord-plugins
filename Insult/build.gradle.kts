83c11456506cc7f99f9d633a6ddcabf0fff7f0ac
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-01-28 16:43:10 -0800

    feat: deprecate Insult plugin

--- Insult/build.gradle.kts
+++ Insult/build.gradle.kts
@@ -1,9 +1,12 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Send insults with /insult"
 
 aliucord {
 	changelog.set(
 		"""
+			# 1.0.1
+			* Deprecated
+
 			# 1.0.0
 			* Released
 		""".trimIndent()
--- Insult/src/main/kotlin/com.github.diamondminer88.plugins/Insult.kt
+++ Insult/src/main/kotlin/com.github.diamondminer88.plugins/Insult.kt
@@ -1,24 +1,25 @@
 package com.github.diamondminer88.plugins
 
 import android.content.Context
-import com.aliucord.Http
+import android.widget.Toast
+import com.aliucord.Constants
 import com.aliucord.annotations.AliucordPlugin
-import com.aliucord.api.CommandsAPI.CommandResult
 import com.aliucord.entities.Plugin
+import java.io.File
 
 @Suppress("unused")
 @AliucordPlugin
+@Deprecated("This plugin is deprecated")
 class Insult : Plugin() {
 	override fun start(ctx: Context) {
-		commands.registerCommand(
-			"insult",
-			"Send an insult",
-		) {
-			CommandResult(Http.simpleGet("https://insult.mattbas.org/api/insult"))
-		}
+		Toast.makeText(
+			ctx,
+			"The Insult plugin no longer functional. It has been automatically removed.",
+			Toast.LENGTH_LONG
+		).show()
+
+		File(Constants.PLUGINS_PATH, "Insult.zip").delete()
 	}
 
-	override fun stop(context: Context) {
-		commands.unregisterAll()
-	}
+	override fun stop(context: Context) {}
 }
